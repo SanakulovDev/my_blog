d5d08969c4c2a17162ac8ea7d7519f75ef951194
SanakulovDev <sanakulovanvar2001@gmail.com>
2024-07-29 05:11:39 +0500

    some changes

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,1 @@
+<h2>Salom dunyo <?=$id?></h2>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/site/index/{id}/user', function ($id) {
+    return view('site/index');
 });
