2b8a6413b15d60bd705d2e0d19b36db70e208047
Sanakulov <72655060+SanakulovDev@users.noreply.github.com>
2024-09-15 12:43:28 +0500

    Delete docker directory

--- docker/php/run-daemons.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cron -f &
-# uncomment if you use workers
-#service supervisor start
-#supervisorctl reread
-#supervisorctl update
-#supervisorctl start messenger-consume:*
-docker-php-entrypoint php-fpm
